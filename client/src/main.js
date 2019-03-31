9730cc78acfaa08f02f0e15c33159ec7a93b5ca4
Tree Casiano <tree.pdx@gmail.com>
2019-03-30 17:38:30 -0700

    Add babel polyfill and Vuetify packages

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import "babel-polyfill";
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vuetify from "vuetify";
+import "../node_modules/vuetify/dist/vuetify.min.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Vuetify);
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+init();
+
+async function init() {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount("#app");
+}
--- client/src/router.js
+++ client/src/router.js
@@ -1,23 +1,28 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-})
+});
+
+// TODO: Implement a NotFoundComponent (catch-all route) to show a 404 page
+// see: https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
