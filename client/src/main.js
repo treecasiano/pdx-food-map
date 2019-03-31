780b8ba15e4e46eae87034979709840c9d87ed4d
Tree Casiano <tree.pdx@gmail.com>
2019-03-30 18:27:28 -0700

    Add example map view

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,29 @@ import router from "./router";
 import store from "./store";
 import Vuetify from "vuetify";
 import "../node_modules/vuetify/dist/vuetify.min.css";
+import L from "leaflet";
+delete L.Icon.Default.prototype._getIconUrl;
+// import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+
+Vue.component("l-map", LMap);
+Vue.component("l-tile-layer", LTileLayer);
+Vue.component("l-marker", LMarker);
+
+// Icon.Default.mergeOptions({
+//   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+//   iconUrl: require("leaflet/dist/images/marker-icon.png"),
+//   shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+// });
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
+
+// delete Icon.Default.prototype._getIconUrl;
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
---
+++ client/src/views/Map.vue
@@ -0,0 +1,51 @@
+<template>
+  <div style="height: 100%; width: 100%">
+    <div
+      class="info"
+      style="height: 15%"
+    >
+      <span>Center: {{ center }}</span>
+      <span>Zoom: {{ zoom }}</span>
+      <span>Bounds: {{ bounds }}</span>
+    </div>
+    <l-map
+      style="height: 700px; width: 100%"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      ></l-tile-layer>
+    </l-map>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      zoom: 10.5,
+      center: [45.5155, -122.6793],
+      bounds: null,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    };
+  },
+  methods: {
+    zoomUpdated(zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated(center) {
+      this.center = center;
+    },
+    boundsUpdated(bounds) {
+      this.bounds = bounds;
+    }
+  }
+}
+</script>
