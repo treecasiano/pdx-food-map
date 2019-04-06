c281c26ff59ed011b52ebd6bd1dbd7ba5f99307a
Tree Casiano <tree.pdx@gmail.com>
2019-04-06 08:28:55 -0700

    Add grocery store routes and tests

---
+++ __tests__integration__/groceryStoreGeoJSON/GET.spec.js
@@ -0,0 +1,38 @@
+const axios = require("axios");
+
+test("Retrieves a geojson object (grocery store point features)", async () => {
+  try {
+    const results = await axios({
+      method: "get",
+      url: "http://localhost:3000/api/groceryStoreGeoJSON"
+    });
+    expect(results.status).toEqual(200);
+    expect(results.data).toBeInstanceOf(Object);
+    expect(results.data.features.length).toEqual(105);
+    expect(results.data.type).toEqual("FeatureCollection");
+    expect(results.data.features[0].type).toEqual("Feature");
+    expect(results.data.features[0]).toEqual({
+      geometry: {
+        coordinates: [-122.697560163699, 45.5773330459487],
+        type: "Point"
+      },
+      id: 1,
+      properties: {
+        address: "3011 N LOMBARD ST",
+        bldg_sqft: 5823,
+        name: "Green Zebra Grocery",
+        objectid: 101,
+        state_id: "1N1E09CC  5600",
+        status: "Existing",
+        type: "Independent or Ethnic Grocery",
+        x: 7639467.013063886,
+        y: 704231.9593578038
+      },
+      type: "Feature"
+    });
+    expect(results.data.type).toEqual("FeatureCollection");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+});
---
+++ routes/groceryStoreGeoJSON.js
@@ -0,0 +1,33 @@
+function factory(logger, groceryStoreService) {
+  GET.apiDoc = {
+    summary: "Get a GeoJSON object (PDX Metro grocery stores)",
+    tags: ["PDX Metro Grocery Stores"],
+    produces: ["application/json"],
+    parameters: [],
+    responses: {
+      200: {
+        description: "Polygon features in geojson format"
+      },
+      500: {
+        description: "Server Error"
+      }
+    }
+  };
+
+  return {
+    GET
+  };
+
+  async function GET(req, res) {
+    let result;
+    try {
+      result = await groceryStoreService.getGeoJSON();
+    } catch (e) {
+      logger.error(e);
+      return res.status(500).json({ message: "Database error" });
+    }
+    return res.status(200).json(result);
+  }
+}
+
+module.exports = factory;
