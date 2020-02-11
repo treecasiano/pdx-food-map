7ed01ce5d30f2da25c5162e12d9665618290d1b3
Tree Casiano <tree.pdx@gmail.com>
2020-02-10 22:15:24 -0800

    Add PUT and POST for Grocery Store

--- routes/groceryStore.js
+++ routes/groceryStore.js
@@ -1,7 +1,7 @@
 function factory(logger, groceryStoreService) {
   GET.apiDoc = {
     summary:
-      "Returns a list of all grocery stores in the PDX-Vancouver-Hillsboro MSA",
+      "Returns a list of all Grocery Stores in the PDX-Vancouver-Hillsboro MSA",
     tags: ["PDX Metro Grocery Stores"],
     produces: ["application/json"],
     parameters: [],
@@ -15,8 +15,73 @@ function factory(logger, groceryStoreService) {
     }
   };
 
+  POST.apiDoc = {
+    summary: "Creates a Grocery Store",
+    tags: ["PDX Metro Grocery Stores"],
+    produces: ["application/json"],
+    parameters: [
+      {
+        description: "Record attributes",
+        in: "body",
+        name: "groceryStore",
+        required: true,
+        schema: {
+          properties: {
+            name: {
+              description: "Grocery Store Name",
+              type: "string"
+            },
+            address: {
+              description: "Grocery Store Location",
+              type: "string"
+            },
+            bldg_sqft: {
+              description: "Building Square Feet",
+              type: "string"
+            },
+            status: {
+              description: "Status",
+              type: "string",
+              enum: ["Existing", "Closed"]
+            },
+            type: {
+              description: "Type of Store",
+              type: "string",
+              enum: ["Large Chain Grocery", "Independent or Ethnic Grocery"]
+            },
+            longitude: {
+              description: "longitude",
+              type: "number"
+            },
+            latitude: {
+              description: "latitude",
+              type: "number"
+            }
+          },
+          required: ["name", "latitude", "longitude"],
+          type: "object"
+        }
+      }
+    ],
+    responses: {
+      200: {
+        description: "Successfully created a new record"
+      },
+      400: {
+        description: "Bad Request"
+      },
+      500: {
+        description: "Internal Server Error"
+      },
+      default: {
+        description: "An error occured"
+      }
+    }
+  };
+
   return {
-    GET
+    GET,
+    POST
   };
 
   async function GET(req, res) {
@@ -28,6 +93,24 @@ function factory(logger, groceryStoreService) {
       return res.status(500).json({ message: "Server Error" });
     }
     return res.status(200).json(result);
+  }
+
+  async function POST(req, res) {
+    const { body: params } = req;
+    let result;
+    if (Object.keys(params).length < 1) {
+      return res
+        .status(400)
+        .json({ message: "Must include at least one attribute" });
+    }
+    try {
+      const { gid } = await groceryStoreService.create(params);
+      result = await groceryStoreService.get(gid);
+    } catch (e) {
+      logger.error(e);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+    return res.status(200).json({ result });
   }
 }
 
---
+++ routes/groceryStore/{id}.js
@@ -0,0 +1,180 @@
+function factory(logger, groceryStoreService) {
+  GET.apiDoc = {
+    summary: "Get Grocery Store record by ID",
+    tags: ["PDX Metro Grocery Stores"],
+    produces: ["application/json"],
+    parameters: [
+      {
+        description: "Grocery Store ID",
+        in: "path",
+        minimum: 1,
+        name: "id",
+        required: true,
+        type: "integer"
+      }
+    ],
+    responses: {
+      200: {
+        description: "OK"
+      },
+      404: {
+        description: "Not found"
+      },
+      500: {
+        description: "Server Error"
+      }
+    }
+  };
+
+  PUT.apiDoc = {
+    summary: "Update record",
+    tags: ["PDX Metro Grocery Stores"],
+    produces: ["application/json"],
+    parameters: [
+      {
+        description: "Grocery Store ID",
+        in: "path",
+        minimum: 1,
+        name: "id",
+        required: true,
+        type: "integer"
+      },
+      {
+        description: "updates",
+        in: "body",
+        name: "groceryStore",
+        required: true,
+        schema: {
+          properties: {
+            name: {
+              description: "Grocery Store Name",
+              type: "string"
+            },
+            address: {
+              description: "Grocery Store Location",
+              type: "string"
+            },
+            bldg_sqft: {
+              description: "Building Square Feet",
+              type: "string"
+            },
+            status: {
+              description: "Status",
+              type: "string",
+              enum: ["Existing", "Closed"]
+            },
+            type: {
+              description: "Type of Store",
+              type: "string",
+              enum: ["Large Chain Grocery", "Independent or Ethnic Grocery"]
+            },
+            longitude: {
+              description: "longitude",
+              type: "number"
+            },
+            latitude: {
+              description: "latitude",
+              type: "number"
+            }
+          },
+          type: "object"
+        }
+      }
+    ],
+    responses: {
+      200: {
+        description: "OK"
+      },
+      404: {
+        description: "Not found"
+      },
+      500: {
+        description: "Server Error"
+      }
+    }
+  };
+
+  return {
+    GET,
+    PUT
+  };
+
+  async function GET(req, res) {
+    const {
+      params: { id }
+    } = req;
+
+    let record;
+
+    try {
+      record = await groceryStoreService.get(id);
+    } catch (e) {
+      logger.error(e);
+      return res.status(500).json({ message: "Server Error" });
+    }
+
+    if (!record) {
+      return res.status(404).json({ message: `No record for id ${id}` });
+    }
+
+    return res.status(200).json(record);
+  }
+
+  async function PUT(req, res) {
+    const {
+      params: { id }
+    } = req;
+
+    const updates = req.body;
+
+    if (Object.keys(updates).length < 1) {
+      return res
+        .status(400)
+        .json({ message: "Must update at least one attribute" });
+    }
+
+    let record;
+    let oldLong;
+    let oldLat;
+
+    try {
+      record = await groceryStoreService.get(id);
+      if (!record) {
+        return res
+          .status(404)
+          .json({ message: `Unable to find record for id: ${id}` });
+      }
+      oldLong = record.longitude;
+      oldLat = record.latitude;
+
+      // If only one long/lat value is being updated,
+      // get the other value from the db because PostGIS functions
+      // in the service need both values.
+      if (updates.latitude && !updates.longitude) {
+        updates.longitude = oldLong;
+      }
+
+      if (!updates.latitude && updates.longitude) {
+        updates.latitude = oldLat;
+      }
+    } catch (e) {
+      logger.error(e);
+      return res.status(500).json({ message: "Database error" });
+    }
+
+    let result;
+    try {
+      await groceryStoreService.update(id, updates);
+      result = await groceryStoreService.get(id);
+    } catch (e) {
+      logger.error(e);
+      return res.status(500).json({
+        message: "Database error"
+      });
+    }
+
+    return res.status(200).json({ result });
+  }
+}
+
+module.exports = factory;
