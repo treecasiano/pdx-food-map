817e195187265c25e13f19722e28518681de6060
Tree Casiano <tree.pdx@gmail.com>
2020-02-22 13:24:53 -0800

    Add test for food pantry GET route

---
+++ __tests__integration__/foodPantryGeoJSON/GET.spec.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+
+test("Retrieves a GeoJSON feature collection (food pantry point features)", async () => {
+  try {
+    const results = await axios({
+      method: "get",
+      url: "http://localhost:3000/api/foodPantryGeoJSON"
+    });
+    expect(results.status).toEqual(200);
+    expect(results.data).toBeInstanceOf(Object);
+    expect(results.data.features.length).toEqual(2);
+    expect(results.data.type).toEqual("FeatureCollection");
+    expect(results.data.features[0].type).toEqual("Feature");
+
+    const featureAttributes = ["geometry", "id", "properties", "type"];
+    const propertyAttributes = [
+      "areas_served",
+      "city",
+      "date_created",
+      "hours_of_operation",
+      "latitude",
+      "location_name",
+      "longitude",
+      "phone",
+      "state",
+      "street_address_1",
+      "street_address_2",
+      "website",
+      "zip"
+    ];
+
+    const geojsonFeatureKeys = Object.keys(results.data.features[0]).sort();
+    const geojsonPropertiesKeys = Object.keys(
+      results.data.features[0].properties
+    ).sort();
+
+    expect(geojsonFeatureKeys.sort()).toEqual(featureAttributes);
+    expect(geojsonPropertiesKeys).toEqual(propertyAttributes);
+
+    expect(results.data.type).toEqual("FeatureCollection");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+});
