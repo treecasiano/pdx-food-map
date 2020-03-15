d0c8aed7058220830ae21ad2bc155b50b6d67051
Tree Casiano <tree.pdx@gmail.com>
2020-03-14 22:00:35 -0700

    Add area (sq miles) to tract route and surface in UI

--- client/src/components/MainMap.vue
+++ client/src/components/MainMap.vue
@@ -811,8 +811,13 @@ export default {
       <div>Poverty Rate: <strong><span class="mono-font">${
         props.povertyrat
       }%</span></strong><div>
+      <div>Persons per Square Mile: <strong><span class="mono-font">${(
+        props.pop2010 / props.area_sqmiles
+      ).toFixed(0)}</span></strong><div>
       `;
       return propertyString;
+
+      // {{ (tract.pop2010 / tract.area_sqmiles).toFixed(2)}}
     },
     createPolyline(geoJSON, { color, weight }) {
       const polyLineArray = geoJSON["features"].map(feature => {
